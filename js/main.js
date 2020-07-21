daffd5b0f7fdce67081bbdee8284886a6c273e4e
Eve Keehl <47083007+EveKeehl@users.noreply.github.com>
2020-07-21 10:15:48 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,2 @@
+$('a[href="#"]').on("click",(function(n){n.preventDefault()})),$((function(){$("#menu").slicknav({prependTo:"main-menu"})})),$(".hamburger").on("click",(function(n){$(".hamburger, .main-menu").toggleClass("is-active")}));
+//# sourceMappingURL=main.js.map
