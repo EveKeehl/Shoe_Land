348fa2bfff34a289be531d806ffec4b533be23ba
Eve Keehl <47083007+EveKeehl@users.noreply.github.com>
2020-07-22 10:22:21 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -1,2 +1,2 @@
-$('a[href="#"]').on("click",(function(n){n.preventDefault()})),$((function(){$("#menu").slicknav({prependTo:"main-menu"})})),$(".hamburger").on("click",(function(n){$(".hamburger, .main-menu").toggleClass("is-active")}));
+$('a[href="#"]').on("click",(function(n){n.preventDefault()})),$((function(){$("#menu").slicknav({prependTo:".mobile-menu"})})),$(".hamburger").on("click",(function(n){$(".hamburger, .main-menu").toggleClass("is-active")})),(new WOW).init();
 //# sourceMappingURL=main.js.map
